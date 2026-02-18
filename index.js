860e7dfa48dc8c5766bfaf19e4efc3a742bbbd08
kom511901-ops <kom511901@gmail.com>
2026-02-18 14:07:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ import fetch from "node-fetch";
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://alexcontent.ru");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // если браузер прислал "проверку" (OPTIONS) — отвечаем сразу
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+
+  next();
+});
+
+
 app.get("/", (req, res) => {
   res.send("OK");
 });
